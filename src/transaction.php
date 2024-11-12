7a1de8e3d944eaa9bf8a924148814b1d37050e3f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-12 13:45:22 +0000

    Updated transaction.php

--- src/transaction.php
+++ src/transaction.php
@@ -1,6 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 class Transaction
 {
-  
+  public float $amount = 15;
+  public string $description;
+
+  public function __construct(float $amount, string $description) {
+    $this->amount = $amount;
+    $this->description = $description;
+  }
 }
