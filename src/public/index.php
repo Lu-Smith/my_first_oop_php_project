d348cd57059f835a7377f1d2751190d57f19bb38
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-12 12:39:18 +0000

    Updated index.php
    Added Transaction class

--- src/public/index.php
+++ src/public/index.php
@@ -1,9 +1,7 @@
 <?php
 
-declare(strict_type = 1);
+require_once '../Transaction.php';
 
-phpinfo();
+$transaction = new Transaction();
 
-echo '<pre>';
-print_r($_SERVER);
-echo '</pre>';
+var_dump($transaction);
