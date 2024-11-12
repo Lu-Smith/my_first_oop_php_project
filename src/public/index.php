6564733c3a8dd82cb92334e11bdfa55ec33956aa
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-12 13:45:31 +0000

    Updated index.php

--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 require_once '../Transaction.php';
 
-$transaction = new Transaction();
+$transaction = new Transaction(amount: 14, description: 'Transaction 1');
 
-var_dump($transaction);
+var_dump(value: $transaction->amount);
