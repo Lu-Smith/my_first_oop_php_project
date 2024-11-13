8b9dbfe778a8a2108285d631463559e72c75531c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-13 13:43:24 +0000

    Updated index.php

--- src/public/index.php
+++ src/public/index.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 require_once '../Transaction.php';
 
-$transaction = new Transaction(amount: 14, description: 'Transaction 1');
+$transaction = new Transaction(amount: 100, description: 'Transaction 1');
 
-$transaction->addTax(8);
+$transaction->addTax(rate: 8);
 
-$transaction->applyDiscount(10);
+$transaction->applyDiscount(rate: 10);
 
 var_dump(value: $transaction->amount);
