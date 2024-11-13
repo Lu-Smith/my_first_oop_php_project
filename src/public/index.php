fb592ed4c6526b411eed761a0a6ba85121ff3d89
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-13 17:11:10 +0000

    Updated index.php

--- src/public/index.php
+++ src/public/index.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 require_once '../Transaction.php';
 
-$amount = (new Transaction(amount: 100, description: 'Transaction 1'))
+$transaction1 = (new Transaction(amount: 100, description: 'Transaction 1'))
 ->addTax(rate: 8)
-->applyDiscount(rate: 10)
-->getAmount();
+->applyDiscount(rate: 10);
 
-var_dump(value: $amount);
+$transaction2 = (new Transaction(amount: 1000, description: 'Transaction 2'))
+->addTax(rate: 10)
+->applyDiscount(rate: 15);
+
+var_dump($transaction1->getAmount(), $transaction2->getAmount());
