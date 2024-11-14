0d6b8d69fc15f373a0cbc82f2e98d5c9ff30beda
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-14 21:27:36 +0000

    Updated code with composer

--- src/app/Transaction.php
+++ src/app/Transaction.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+namespace App;
+
 class Transaction
 {
   private float $amount = 15;
--- src/public/index.php
+++ src/public/index.php
@@ -2,13 +2,13 @@
 
 declare(strict_types=1);
 
-require_once '../Transaction.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-$transaction1 = (new Transaction(amount: 100, description: 'Transaction 1'))
+$transaction1 = (new App\Transaction(amount: 100, description: 'Transaction 1'))
 ->addTax(rate: 8)
 ->applyDiscount(rate: 10);
 
-$transaction2 = (new Transaction(amount: 1000, description: 'Transaction 2'))
+$transaction2 = (new App\Transaction(amount: 1000, description: 'Transaction 2'))
 ->addTax(rate: 10)
 ->applyDiscount(rate: 15);
 
