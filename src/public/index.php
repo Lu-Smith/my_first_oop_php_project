9a1fde3a3f31b698ed0eaaf11a821bd17643373f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-14 21:45:03 +0000

    Updated index.php

--- src/public/index.php
+++ src/public/index.php
@@ -2,14 +2,19 @@
 
 declare(strict_types=1);
 
+use App\Enum\Status;
+use App\Transaction;
+
 require __DIR__ . '/../vendor/autoload.php';
 
-$transaction1 = (new App\Transaction(amount: 100, description: 'Transaction 1'))
-->addTax(rate: 8)
-->applyDiscount(rate: 10);
+echo Status::STATUS_PAID;
 
-$transaction2 = (new App\Transaction(amount: 1000, description: 'Transaction 2'))
-->addTax(rate: 10)
-->applyDiscount(rate: 15);
+// $transaction1 = (new App\Transaction(amount: 100, description: 'Transaction 1'))
+// ->addTax(rate: 8)
+// ->applyDiscount(rate: 10);
 
-var_dump($transaction1->getAmount(), $transaction2->getAmount());
+// $transaction2 = (new App\Transaction(amount: 1000, description: 'Transaction 2'))
+// ->addTax(rate: 10)
+// ->applyDiscount(rate: 15);
+
+// var_dump($transaction1->getAmount(), $transaction2->getAmount());
